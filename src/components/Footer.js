e255c4b795c658be2ea37055ded52c3f8e2f74e3
Edison-choo <chooedison@yahoo.com>
2022-03-26 21:35:03 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="row">
 
       <div className="col-lg-3 col-md-6 footer-contact">
-        <h3>Ninestars</h3>
+        <h3>BlockVote</h3>
         <p>A108 Adam Street <br/>New York, NY 535022<br/>United States <br/><br/><strong>Phone:</strong> +1 5589 55488 55<br/><strong>Email:</strong> info@example.com<br/>
         </p>
       </div>
